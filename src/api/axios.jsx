de59eea3446f2ca300f2bc9f588531d747ca00d7
anto254 <jenga.anto@gmail.com>
2026-09-15 21:06:19 +0300

    chore: switch API base URL to production endpoint

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const BASE_URL = "https://api.sheetresolve.com/api";
- const BASE_URL = "http://localhost:3500/api";
+const BASE_URL = "https://api.sheetresolve.com/api";
+//  const BASE_URL = "http://localhost:3500/api";
 
 export default axios.create({
   baseURL: BASE_URL,
